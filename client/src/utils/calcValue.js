c5f98a764cb7a06b368ddda70c4192aee813d9d0
Stanislav Kad <stanislav.kad@gmail.com>
2023-08-23 22:03:12 +0200

    calc formula refactoring

--- client/src/utils/calcValue.js
+++ client/src/utils/calcValue.js
@@ -4,11 +4,8 @@ const calculateExchangeRate = (sourceCurrency, targetCurrency, exchangeRateData)
     const baseCurr = exchangeRateData.Abase
     
     const baseCurrencyRate = exchangeRateData.rates[baseCurr];
-
-    const baseToSourceRate = 1 / exchangeRateData.rates[sourceCurrency];
-    const baseToTargetRate = 1 / exchangeRateData.rates[targetCurrency];
     
-    const exchangeRate = (baseToSourceRate / baseToTargetRate ) * baseCurrencyRate;
+    const exchangeRate = (exchangeRateData.rates[targetCurrency] / exchangeRateData.rates[sourceCurrency] ) * baseCurrencyRate;
     
     return exchangeRate;
 };
